754a07cb162ac7ee0f44e13f19b348068ba36093
ramoma <monecraftisgood@gmail.com>
2026-09-22 15:32:01 +0800

    slight changes

--- src/viewers/Window.java
+++ src/viewers/Window.java
@@ -1,15 +1,25 @@
 package viewers;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Container;
 
 public class Window extends JFrame{
+	
+	Container c = getContentPane();
 
 	public Window(){
 		
-		setSize(500,800);
+		setTitle("My Little Ghibli");
+		setSize(512,917);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
 		
+		
+	}
+	
+	void createPlayBar() {
+		//passthis
 	}
 	
 }
