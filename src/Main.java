af14fd0d2fb4ca0f8508e1e2373d9b24de2c991b
ramoma <monecraftisgood@gmail.com>
2026-09-22 15:07:52 +0800

    added frame

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import viewers.Window;
+
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		Window window = new Window();
+		
+	}
+	
+}
---
+++ src/viewers/Window.java
@@ -0,0 +1,15 @@
+package viewers;
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+
+	public Window(){
+		
+		setSize(500,800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setVisible(true);
+		
+	}
+	
+}
