916f5bfce66b237fb5adec245452a51454979979
ramoma <monecraftisgood@gmail.com>
2026-09-23 23:55:28 +0800

    TODO: finish window components

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import viewers.Window;
 
 
-
 public class Main {
 	
 	public static void main(String[] args) {
--- src/viewers/Window.java
+++ src/viewers/Window.java
@@ -1,11 +1,14 @@
 package viewers;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.Container;
+import javax.swing.JButton;
+
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.BorderLayout;
+
 
 public class Window extends JFrame{
-	
-	Container c = getContentPane();
 
 	public Window(){
 		
@@ -13,13 +16,42 @@ public class Window extends JFrame{
 		setSize(512,917);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
+		setLayout(new BorderLayout());
+		
+		createPlayBar();
+		createActionsMenu();
+		
 		setVisible(true);
+	
 		
 		
 	}
 	
 	void createPlayBar() {
-		//passthis
+		
+		JPanel mainContent = new JPanel();
+		JPanel topBar = new JPanel();
+		
+		topBar.setPreferredSize(new Dimension(412, 46));
+		
+		mainContent.setPreferredSize(new Dimension(412, 553));
+		mainContent.setBackground(Color.RED);
+		
+		
+		getContentPane().add(mainContent, BorderLayout.CENTER);
+		getContentPane().add(topBar, BorderLayout.NORTH);
+		
+	}
+	
+	void createActionsMenu() {
+		
+		JPanel mainBody = new JPanel();
+		
+		mainBody.setPreferredSize(new Dimension(412, 251));
+		mainBody.setBackground(Color.ORANGE);
+		
+		getContentPane().add(mainBody, BorderLayout.SOUTH);
+		
 	}
 	
 }
